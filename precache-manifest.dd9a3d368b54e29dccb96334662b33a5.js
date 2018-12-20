727a5958dbae558c5b35f452fda471a01dba494e
Gerald Haxhillari <haxhillari@outlook.com>
2018-12-20 20:01:24 +0100

    Updates

---
+++ precache-manifest.dd9a3d368b54e29dccb96334662b33a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f726a0ea515ef25c0f39",
+    "url": "/myreact/static/js/runtime~main.f726a0ea.js"
+  },
+  {
+    "revision": "3061cb1f879d2146371b",
+    "url": "/myreact/static/js/main.3061cb1f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/myreact/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3061cb1f879d2146371b",
+    "url": "/myreact/static/css/main.6ddeda21.chunk.css"
+  },
+  {
+    "revision": "6c1ac240b8930e2faa52ff8564406eb0",
+    "url": "/myreact/index.html"
+  }
+];
